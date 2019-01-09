77055f2cbe02dcac0c4b92dc54366dc55b89a668
A-Morell <46518490+A-Morell@users.noreply.github.com>
2019-01-09 09:21:13 +0100

    Update TFM_app.R

--- TFM_app.R
+++ TFM_app.R
@@ -45,18 +45,6 @@ require(org.EcSakai.eg.db)
 
 require(TxDb.Hsapiens.UCSC.hg19.knownGene)
 
-# Datos de prueba
-
-data(geneList, package="DOSE")
-genes <- names(geneList)[abs(geneList) > 2]
-
-files_NGS <- getSampleFiles()
-peak <- readPeakFile(files[[4]])
-genes_NGS <- seq2gene(peak, tssRegion = c(-1000, 1000), flankDistance = 3000, TxDb = txdb)
-
-write.table(genes, file = "genes.txt", sep = "\t", dec = ".", col.names = FALSE, row.names = FALSE)
-write.table(genes_NGS, file = "genes_NGS.txt", sep = "\t", dec = ".", col.names = FALSE, row.names = FALSE)
-
 
 
 
