07a5602e5889b7ddef49f56441f1c892572a6124
A-Morell <46518490+A-Morell@users.noreply.github.com>
2019-01-09 09:19:38 +0100

    Update TFM_app.R

--- TFM_app.R
+++ TFM_app.R
@@ -11,17 +11,6 @@
 
 
 
-#Instalación de paquetes
-
-install.packages(c("shiny", "shinydashboard", "utils"), dep = TRUE)
-
-source("http://bioconductor.org/biocLite.R")
-biocLite(c("clusterProfiler", "DOSE", "GOSemSim", "ChIPseeker", "pathview", "org.Hs.eg.db", "org.Mm.eg.db",
-           "org.Rn.eg.db", "org.Sc.sgd.db", "org.Dm.eg.db", "org.Dr.eg.db", "org.At.eg.db", "org.Bt.eg.db",
-           "org.Ce.eg.db", "org.Gg.eg.db", "org.Cf.eg.db", "org.Ss.eg.db", "org.Mmu.eg.db", "org.Eck12.eg.db",
-           "org.Ag.eg.db", "org.Xl.eg.db", "org.Pt.eg.db", "org.Pf.plasmo.db", "org.EcSakai.eg.db",
-           "TxDb.Hsapiens.UCSC.hg19.knownGene"))
-
 ## Carga de los paquetes
 
 require(shiny)
