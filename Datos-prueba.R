2f7f2d956e82d07da421eb109d325d3b28a437ba
A-Morell <46518490+A-Morell@users.noreply.github.com>
2019-01-12 09:32:56 +0100

    Update Datos-prueba.R

--- Datos-prueba.R
+++ Datos-prueba.R
@@ -4,8 +4,8 @@ data(geneList, package="DOSE")
 genes <- names(geneList)[abs(geneList) > 2]
 
 files_NGS <- getSampleFiles()
-peak <- readPeakFile(files[[4]])
-genes_NGS <- seq2gene(peak, tssRegion = c(-1000, 1000), flankDistance = 3000, TxDb = txdb)
+peak <- readPeakFile(files_NGS[[4]])
+genes_NGS <- seq2gene(peak, tssRegion = c(-1000, 1000), flankDistance = 3000, TxDb = TxDb.Hsapiens.UCSC.hg19.knownGene)
 
 write.table(genes, file = "genes.txt", sep = "\t", dec = ".", col.names = FALSE, row.names = FALSE)
 write.table(genes_NGS, file = "genes_NGS.txt", sep = "\t", dec = ".", col.names = FALSE, row.names = FALSE)
