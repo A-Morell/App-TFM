13ab4b488a70d2ac4dd79a30c87ea46d05dc7b29
A-Morell <46518490+A-Morell@users.noreply.github.com>
2019-01-12 12:30:26 +0100

    Update Datos-prueba.R

--- Datos-prueba.R
+++ Datos-prueba.R
@@ -4,12 +4,13 @@
 data(geneList, package="DOSE")
 genes <- names(geneList)[abs(geneList) > 2]
 
-write.table(genes, file = "genes.txt", sep = "\t", dec = ".", col.names = FALSE, row.names = FALSE)
+write.table(genes, file = "genes.txt", sep = "\t", col.names = FALSE, row.names = FALSE)
 
+# Para crear el universo de genes
+write.table(geneList, file = "universe.txt", sep = "\t", col.names = FALSE, row.names = FALSE)
 
 # Para crear un lista de picos
 files_NGS <- getSampleFiles()
-peak <- readPeakFile(files_NGS[[4]])
-genes_NGS <- seq2gene(peak, tssRegion = c(-1000, 1000), flankDistance = 3000, TxDb = TxDb.Hsapiens.UCSC.hg19.knownGene)
+peaks <- readPeakFile(files_NGS[[4]])
 
-write.table(genes_NGS, file = "genes_NGS.txt", sep = "\t", dec = ".", col.names = FALSE, row.names = FALSE)
+write.table(peaks, file = "peaks.txt", sep = "\t", dec = ".", col.names = FALSE, row.names = FALSE)
